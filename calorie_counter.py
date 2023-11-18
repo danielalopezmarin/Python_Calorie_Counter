956e82420887824973e5e17b5245133918a1695b
danielalopezmarin <danielalopez.marin7@gmail.com>
2023-11-19 00:06:27 +0100

    Add

--- calorie_counter.py
+++ calorie_counter.py
@@ -1,4 +1,4 @@
-calories = {
+meals = {
    'Hamburger': 600,
    'Cheese Burger': 750,
    'Veggie Burger': 400,
@@ -8,3 +8,21 @@ calories = {
    'Iced Tea': 70,
    'Lemonade': 90,
 }
+
+combos = {
+    "Cheesy Combo": ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
+    "Veggie Combo": ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
+    "Vegan Combo": ["Vegan Burger", "Salad", "Lemonade"],
+}
+
+
+def calorie_counter(*items):
+    total = 0
+    for item in items:
+        if item in meals:
+            total += meals [item]
+        elif item in combos:
+            total += calorie_counter (*combos [item])
+        else:
+            print (f"{item} not found in the menu")
+    return total
