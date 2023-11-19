3b58df47ee6eb04be5ab235128628cc57e4a2194
danielalopezmarin <danielalopez.marin7@gmail.com>
2023-11-19 01:39:03 +0100

    Add

--- module/calorie_counter.py
+++ module/calorie_counter.py
@@ -1,22 +1,5 @@
+from module.data import meals, combos
 from module.exceptions import MealTooBigError
-
-meals = {
-   'Hamburger': 600,
-   'Cheese Burger': 750,
-   'Veggie Burger': 400,
-   'Vegan Burger': 350,
-   'Sweet Potatoes': 230,
-   'Salad': 15,
-   'Iced Tea': 70,
-   'Lemonade': 90,
-}
-
-combos = {
-    "Cheesy Combo": ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
-    "Veggie Combo": ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
-    "Vegan Combo": ["Vegan Burger", "Salad", "Lemonade"],
-}
-
 
 def calorie_counter(*items):
     total = 0
